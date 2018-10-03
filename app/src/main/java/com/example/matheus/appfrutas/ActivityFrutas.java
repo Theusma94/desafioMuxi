1a25fde9195ed52dd3e57619e1a3aa96db0cfbd3
Matheus Lima <matheuslima9412@gmail.com>
2018-10-03 14:41:59 -0300

    inserção do framework Volley para o consumo do json

--- app/src/main/java/com/example/matheus/appfrutas/ActivityFrutas.java
+++ app/src/main/java/com/example/matheus/appfrutas/ActivityFrutas.java
@@ -7,7 +7,17 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.matheus.appfrutas.adapters.FrutasAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,25 +25,62 @@ import java.util.List;
 public class ActivityFrutas extends AppCompatActivity {
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
-    private ArrayList<String> content;
+    private RequestQueue mQueue;
+    private JSONArray jsonArray = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frutas);
-        content = new ArrayList<>();
-        for (int i = 0; i<12;i++){
-            content.add("item"+(i+1));
-        }
+        // Cria uma nova requisição utilizando o framework Volley
+        mQueue = Volley.newRequestQueue(this);
 
-
+        //Configurações do recyclerview e layoutmanager
         recyclerView =  findViewById(R.id.frutas);
         recyclerView.setHasFixedSize(true); //significa que o tamanho do RecyclerView não vai mudar
         layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
-        FrutasAdapter adapter = new FrutasAdapter(this,content);
-        recyclerView.setAdapter(adapter);
+
+
+        consomeJson();
+
+    }
+
+    public void consomeJson(){
+        String url = "https://raw.githubusercontent.com/muxidev/desafio-android/master/fruits.json";
+        //Cria a requisição do tipo GET
+        JsonObjectRequest  request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    /*Caso a busca seja feita com sucesso
+                    * é retirado do JSONObject o array de nome 'fruits'
+                    * que é o unico presente na url
+                    */
+                     jsonArray = response.getJSONArray("fruits");
+
+                     /*Cria-se um adapter com passando por parametro o contexto e o JSONArray obtido da url
+                     * em seguida o adapter é setado no recyclerView
+                      */
+                     FrutasAdapter adapter = new FrutasAdapter(ActivityFrutas.this,jsonArray);
+                     recyclerView.setAdapter(adapter);
+
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+
     }
 }
--- app/src/main/java/com/example/matheus/appfrutas/adapters/FrutasAdapter.java
+++ app/src/main/java/com/example/matheus/appfrutas/adapters/FrutasAdapter.java
@@ -11,41 +11,50 @@ import android.widget.Toast;
 
 import com.example.matheus.appfrutas.R;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 /**
  * Created by Matheus on 02/10/2018.
  */
 
 public class FrutasAdapter extends RecyclerView.Adapter<FrutasAdapter.MyViewHolder>  {
-    private LayoutInflater layoutInflater;
-    private ArrayList<String> content;
+    private JSONArray jsonfruits;
     private Context ctx;
-    public FrutasAdapter(Context ctx,ArrayList<String> content)
+    public FrutasAdapter(Context ctx,JSONArray jsonfruits)
     {
         this.ctx = ctx;
-        this.content = content;
+        this.jsonfruits = jsonfruits;
 
 
     }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
     {
-        Log.i("LOG","onCreateViewHolder()");
         return new MyViewHolder(LayoutInflater.from(ctx).inflate(R.layout.item_frutas,parent,false));
     }
 
     @Override
     public void onBindViewHolder(FrutasAdapter.MyViewHolder holder, int position) {
+            try {
+                if(jsonfruits.getJSONObject(position)!=null) {
+                    JSONObject fruits = jsonfruits.getJSONObject(position);
+                    holder.nome.setText(fruits.getString("name"));
+                }
 
-        holder.nome.setText(content.get(position));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
 
     }
 
     @Override
     public int getItemCount() {
-        return content.size();
+        return jsonfruits.length();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
